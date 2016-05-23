8c66bbf09e9ee7701dd2eee3c9b47c093473bd57
Josh Campion <josh@thedistance.co.uk>
2016-05-23 16:55:37 +0100

    Initial Commit

---
+++ TheDistanceComponents/TheDistanceComponents.h
@@ -0,0 +1,19 @@
+//
+//  TheDistanceComponents.h
+//  TheDistanceComponents
+//
+//  Created by Josh Campion on 23/05/2016.
+//  Copyright © 2016 The Distance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TheDistanceComponents.
+FOUNDATION_EXPORT double TheDistanceComponentsVersionNumber;
+
+//! Project version string for TheDistanceComponents.
+FOUNDATION_EXPORT const unsigned char TheDistanceComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TheDistanceComponents/PublicHeader.h>
+
+
